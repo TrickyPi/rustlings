0f236e876c6e7485965627ec2f41f8b115f241a8
TrickyPi <530257315@qq.com>
2022-03-16 00:01:25 +0800

    finishing

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,7 +1,7 @@
 // errors4.rs
 // Make this test pass! Execute `rustlings hint errors4` for hints :)
 
-// I AM NOT DONE
+use std::convert::TryInto;
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,7 +14,12 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value))
+        if value == 0 {
+            return Err(CreationError::Zero);
+        } else if value < 0 {
+            return Err(CreationError::Negative);
+        }
+        Ok(PositiveNonzeroInteger(value.try_into().unwrap()))
     }
 }
 
